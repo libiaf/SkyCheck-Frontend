dfd32d466e25f17819d1e8a0945a88694ce0acfa
marianacarhol <marianacarrilloholguin@outlook.com>
2025-10-04 16:51:56 -0700

    feat: added alerts section

--- SkyCheck/src/pages/principal.tsx
+++ SkyCheck/src/pages/principal.tsx
@@ -1,4 +1,7 @@
 import Dashboard from '../components/dashboard';
+import WindOverview from '../components/WindOverview';
+import ReactDatePicker from '../components/datepicker';
+import Alerts from '../components/Alerts';
 import '../styles/principal.css';
 
 const Principal = () => {
@@ -9,6 +12,7 @@ const Principal = () => {
             </div>
             <div> {/* content */} 
                 <div> {/* left content, wet+date+hours */}
+                    {/*<ReactDatePicker></ReactDatePicker>*/}
                     <div> {/* wet */}
                     
                     </div>
@@ -23,6 +27,13 @@ const Principal = () => {
                 </div>
                 <div className="principal-container d-flex justify-content-end align-items-end">
                 <Dashboard ciudad="Hermosillo" >
+                <Alerts
+                    items={[
+                        "Warning: High UV index expected.",
+                        "Warning: Strong winds after 3 PM."
+                    ]}
+                />
+                    <WindOverview velocity={15} direction={0} gusts={25} />
                 </Dashboard>
                 </div>
             </div>
