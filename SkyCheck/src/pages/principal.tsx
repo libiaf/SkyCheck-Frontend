f2b73990a3c710da7f4d4b6219224bd6c67976fe
Mariana Islas <marianaislasm2004@gmail.com>
2025-10-04 13:17:40 -0700

    feat: Añadí estructura a la página principal

--- SkyCheck/src/pages/principal.tsx
+++ SkyCheck/src/pages/principal.tsx
@@ -3,9 +3,29 @@ import '../styles/principal.css';
 
 const Principal = () => {
     return (
-        <div className="principal-container d-flex justify-content-end align-items-end">
-            <Dashboard ciudad="Hermosillo" >
-            </Dashboard>
+        <div> {/* mapa */}
+            <div> {/* nav bar */}
+                
+            </div>
+            <div> {/* content */} 
+                <div> {/* left content, wet+date+hours */}
+                    <div> {/* wet */}
+                    
+                    </div>
+                    <div> {/* date + hours */}
+                        <div> {/* date */}
+                            
+                        </div>
+                        <div> {/* hours */}
+
+                        </div>
+                    </div>
+                </div>
+                <div className="principal-container d-flex justify-content-end align-items-end">
+                <Dashboard ciudad="Hermosillo" >
+                </Dashboard>
+                </div>
+            </div>
         </div>
     );
 };
